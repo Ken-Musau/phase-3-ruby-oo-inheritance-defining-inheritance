99a5213e183c5d2de8afa65490c63ee09f41e2bd
Kennedy Musau <kennedy.musauu@gmail.com>
2023-07-05 22:30:59 +0300

    completed

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,20 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
-require_relative './vehicle'
+require_relative "./vehicle"
+
+class Car < Vehicle
+  def initialize(make, model)
+    @make = make
+    @model = model
+  end
+
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+end
+
+tesla = Car.new(
+  "tesla", "model s"
+)
+puts Car.superclass
+puts Car.superclass.superclass
+puts Car.superclass.superclass.superclass
